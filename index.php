be33b4ba35af9658eac8817bf8740926a1057082
Denise-Dichiara <dichiara.d@gmail.com>
2023-06-30 19:36:57 +0200

    draft php

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php 
+$saluto = 'ciao';
+//*commento inphp
+?>
+
+
+<!DOCTYPE html>
+<html>
+    <head lang="en">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Php Intro</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+
+        <h1>
+            <?php echo $saluto ?> , benvenuto in php!
+        </h1>
+        
+        <script src="" async defer></script>
+    </body>
+</html>
+
+
