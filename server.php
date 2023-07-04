de052f08c11fff444c2b7b13d19a0ef72b9d29e7
Denise-Dichiara <dichiara.d@gmail.com>
2023-07-04 09:12:18 +0200

    modified new paragraph

--- server.php
+++ server.php
@@ -1,9 +1,57 @@
-<?php 
+<?php
 
 $paragraph = $_GET['userParagraph'];
 
 $censored = $_GET['censored'];
 
-$censoredParagraph = str_replace($badword, '*', $paragraph );
+$censoredParagraph = str_replace($badword, '*', $paragraph);
 
 ?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Badwords results</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="">
+</head>
+
+
+<body>
+    <h1>
+        Badwords results:
+    </h1>
+
+    <h2>
+        Original paragraph:
+    </h2>
+    <p>
+        <?php
+        echo $paragraph
+        ?>
+    </p>
+
+
+    <h2>
+        Censored paragraph:
+    </h2>
+    <p>
+        <?php
+        echo $censoredParagraph
+        ?>
+    </p>
+
+    <p>
+        New paragraph lenght: <?php echo strlen($censoredParagraph);  ?>
+    </p>
+
+
+
+    <script src="" async defer></script>
+</body>
+
+</html>
